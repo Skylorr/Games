f831a0f7b74a660174b2511027b06975ac059d18
Skylor <sergeigorbachev2000@gmail.com>
2022-11-04 19:18:15 +0300

    Update import random.py

--- import random.py
+++ import random.py
@@ -1,15 +1,27 @@
 import random
 
-lowDiapazon = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 highDiapazon = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400]
+playGame = True
+count = 0
+right = 0
+score = 0
 
 print("""Компьютер составляет пример. Введите ответ.""")
 print("*" * 40)
-x = random.choice(highDiapazon)
-y = int(input(f"Корень из {x}?" )) #x вопрос
-z = int(x ** 0.5)
 
-if y == z:
-    print("правильно")
-else:
-    print("Неправильно")
+while (playGame):
+    print(f"Ваши очки: {score}")
+    print(f"Обработано Задач: {count}")
+    print(f"Правильных ответов: {right}")
+    print("-" * 20)
+    
+    x = random.choice(highDiapazon)
+    y = int(input(f"Корень из {x}? " )) #x вопрос
+    z = int(x ** 0.5)
+    
+    if y == z:
+        print("\nПравильно!\n")
+        right += 1
+        score += 10
+    else:
+        print("\nНеправильно!\n")
